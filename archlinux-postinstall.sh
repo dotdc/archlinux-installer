fc6449a7297f84367ebb137add41ec32de69a234
David <davidcalvertfr@gmail.com>
2022-01-25 08:22:43 +0100

    feat: added chrome-gnome-shell / removed terragrunt
    Signed-off-by: David <davidcalvertfr@gmail.com>

--- archlinux-postinstall.sh
+++ archlinux-postinstall.sh
@@ -105,6 +105,7 @@ sudo -u ${username} yay -Sy --color auto $(tr '\n' ' ' < /opt/config-aur-package
 echo -e "[${B}INFO${W}] Enable systemctl services"
 systemctl enable gdm
 systemctl enable NetworkManager
+systemctl enable nftables
 systemctl enable docker
 
 # Set Gnome default favorites apps
